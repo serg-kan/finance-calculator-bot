c2e3ea8d0438cd2d253bba98782915a38bedce1b
serg-kan <serg.kan096@gmail.com>
2023-07-22 08:10:11 +0400

    feat: added categories

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import logging
 import os
 from dotenv import load_dotenv
 
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
@@ -11,11 +10,13 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-from sqlite import db_start, create_profile, add_record, get_records_month
+from sqlite import db_start, add_user, add_record, get_records_month, get_categories
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+
+sum_spent = 0
 
 async def on_startup(_):
     await db_start()
@@ -24,11 +25,26 @@ storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 
+
+### keyboards ###
 def get_menu_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
 
     kb.add(KeyboardButton('/amount'))
     kb.add(KeyboardButton('/analytics'))
+    return kb
+
+def get_category_keyboard(categories) -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    # counter = 1
+    # keyboard_row = ()
+
+    for category in categories:
+        # if (counter % 3 == 0):
+
+        kb.add(KeyboardButton(category))
+
     return kb
 
 def get_cancel() -> ReplyKeyboardMarkup:
@@ -37,22 +53,48 @@ def get_cancel() -> ReplyKeyboardMarkup:
 class ClientStates(StatesGroup):
     menu = State()
     amount = State()
+    category = State()
     analytics = State()
 
+
+### common handlers ###
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message) -> None:
     await ClientStates.menu.set()
     await message.reply('Выберите пункт', reply_markup=get_menu_keyboard())
-    await create_profile(message.from_user.id, message.from_user.full_name)
+    await add_user(message.from_user.id, message.from_user.full_name)
 
-### Menu handlers
+
+### menu handlers ###
 @dp.message_handler(commands=['amount'], state=ClientStates.menu)
-async def amount_handler(message: types.Message, state:FSMContext) -> None:
+async def add_record_handler(message: types.Message, state: FSMContext) -> None:
     await state.set_state(ClientStates.amount)
     await message.reply('Введите сумму')
 
+@dp.message_handler(state=ClientStates.amount)
+async def amount_handler(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(ClientStates.category)
+    async with state.proxy() as data:
+        data['amount'] = int(message.text)
+
+    categories = await get_categories()
+
+    await message.reply('Выберите категорию', reply_markup=get_category_keyboard(categories))
+
+@dp.message_handler(state=ClientStates.category)
+async def category_handler(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(ClientStates.menu)
+
+    async with state.proxy() as data:
+        data['category'] = message.text
+
+        await add_record(message.from_user.id, data['amount'], data['category'])
+
+    await message.reply('Добавлено', reply_markup=get_menu_keyboard())
+
+
 @dp.message_handler(commands=['analytics'], state=ClientStates.menu)
-async def analytics_handler(message: types.Message, state:FSMContext) -> None: 
+async def analytics_handler(message: types.Message, state: FSMContext) -> None: 
     total_month = await get_records_month(message.from_user.id)
 
     await message.reply(f'В этом месяце вы потратили: {total_month}', reply_markup=get_menu_keyboard())
@@ -61,35 +103,6 @@ async def analytics_handler(message: types.Message, state:FSMContext) -> None:
 async def amount_handler_invalid(message: types.Message):
     return await message.reply("Сумма должна быть числом")
 
-@dp.message_handler(state=ClientStates.amount)
-async def amount_handler(message: types.Message, state:FSMContext) -> None:
-    await state.set_state(ClientStates.menu)
-    global sum_spent
-    sum_spent += int(message.text)
-    await message.reply(f'Вы ввели сумму: {int(message.text)}, общее: {sum_spent}', reply_markup=get_menu_keyboard())
-    await add_record(int(message.text), message.from_user.id)
-
-
-# @dp.message_handler(state='*', commands=['cancel'])
-# async def cancel_handler(message: types.Message, state=FSMContext) -> None:
-#     current_state = await state.get_state()
-#     if current_state is None:
-#         return
-    
-#     await state.finish()
-
-
-# @dp.message_handler(lambda message: message.text.isdigit(), state=ClientStatedGroup.amount)
-# async def amount_handler(message: types.Message, state: FSMContext):
-#     global sum_spent
-
-#     sum_spent += int(message.text)
-#     await state.update_data(age=sum_spent)
-#     await message.reply(f'Вы потратили: {sum_spent}')
-
-
-# @dp.message_handler(Text(equals='Начать работу', ignore_case=True)):
-# async def start_work_handler(message: types.Message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- sqlite.py
+++ sqlite.py
@@ -8,16 +8,27 @@ async def db_start():
         db = sq.connect('new.db')
         cur = db.cursor()
 
-        cur.execute('''CREATE TABLE IF NOT EXISTS profile(
-                    user_id TEXT PRIMARY KEY, 
-                    name TEXT)
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS users(
+                        user_id TEXT PRIMARY KEY, 
+                        name TEXT
+                    )
                 ''')
 
-        cur.execute('''CREATE TABLE IF NOT EXISTS records(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    amount REAL,
-                    user_id TEXT)
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS records(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                        amount REAL,
+                        user_id TEXT
+                    )
+                ''')
+        
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS categories(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT
+                    )
                 ''')
         
         db.commit()
@@ -26,15 +37,15 @@ async def db_start():
     except sq.Error as e:
         print ('Error occured')
 
-async def create_profile(user_id, name):
-    user = cur.execute("SELECT 1 FROM profile where user_id == '{key}'".format(key=user_id)).fetchone()
+async def add_user(user_id, name):
+    user = cur.execute("SELECT 1 FROM users where user_id == '{key}'".format(key=user_id)).fetchone()
 
     if not user:
-        cur.execute('INSERT INTO profile VALUES(?, ?)', (user_id, name))
+        cur.execute('INSERT INTO users VALUES(?, ?)', (user_id, name))
         db.commit()
 
-async def add_record(amount, user_id):
-    print('ADD RECORD', amount, user_id)
+async def add_record(user_id, amount, category):
+    print('ADD RECORD', user_id, amount, category)
     try:
         cur.execute('''
                     INSERT INTO records(amount, user_id) 
@@ -58,5 +69,20 @@ async def get_records_month(user_id):
         db.commit()
 
         return total[0]        
+    except sq.Error as e:
+        print('Error', e)
+
+async def get_categories():
+    try:
+        categories = cur.execute('''
+                        SELECT name FROM categories
+                    ''').fetchall()
+        db.commit()
+
+        categories_format = tuple(category[0] for category in categories)
+
+        print('caterogies', categories_format)
+
+        return categories_format
     except sq.Error as e:
         print('Error', e)
